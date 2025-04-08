5c8045c366c386648a0b76ddeff83fb450498659
Venkata Mahesh Polumuri <venkat2552004@gmail.com>
2025-04-08 19:39:13 +0530

    add comments and fix bugs

--- SecurityService/src/test/java/security/service/SecurityServiceTest.java
+++ SecurityService/src/test/java/security/service/SecurityServiceTest.java
@@ -39,66 +39,61 @@ class SecurityServiceTest {
 
     @Test
     void shouldSetPendingAlarmWhenSensorActivatedWhileArmed() {
+        // Setup alarm status and activate sensor
         when(securityRepository.getAlarmStatus()).thenReturn(AlarmStatus.NO_ALARM);
-
         securityService.changeSensorActivationStatus(testSensor, true);
-
+        
+        // Verify alarm status was updated to pending
         verify(securityRepository).setAlarmStatus(AlarmStatus.PENDING_ALARM);
     }
 
     @Test
     void shouldTriggerAlarmWhenSensorActivatedWhilePending() {
-        Sensor sensor = new Sensor("Test Sensor", SensorType.DOOR);
-
+        // Create and set up sensor with pending alarm
+        Sensor doorSensor = new Sensor("Test Sensor", SensorType.DOOR);
         when(securityRepository.getAlarmStatus()).thenReturn(AlarmStatus.PENDING_ALARM);
-        when(securityRepository.getSensors()).thenReturn(Set.of(sensor));
-
-        securityService.changeSensorActivationStatus(sensor, true);
-
+        when(securityRepository.getSensors()).thenReturn(Set.of(doorSensor));
+        
+        // Activate sensor and verify alarm is triggered
+        securityService.changeSensorActivationStatus(doorSensor, true);
         verify(securityRepository).setAlarmStatus(AlarmStatus.ALARM);
     }
 
     @Test
     void shouldResetToNoAlarmWhenAllSensorsInactiveWhilePending() {
+        // Setup active sensor and pending alarm state
         testSensor.setActive(true);
-        Set<Sensor> sensors = new HashSet<>();
-        sensors.add(testSensor);
-
-        when(securityRepository.getSensors()).thenReturn(sensors);
+        Set<Sensor> activeSensors = new HashSet<>();
+        activeSensors.add(testSensor);
+        
+        when(securityRepository.getSensors()).thenReturn(activeSensors);
         when(securityRepository.getAlarmStatus()).thenReturn(AlarmStatus.PENDING_ALARM);
-
+        
+        // Deactivate sensor and verify alarm resets
         securityService.changeSensorActivationStatus(testSensor, false);
-
         verify(securityRepository).setAlarmStatus(AlarmStatus.NO_ALARM);
     }
 
     @Test
     void shouldNotChangeAlarmStateWhenAlarmIsActive() {
         when(securityRepository.getAlarmStatus()).thenReturn(AlarmStatus.ALARM);
-
         securityService.changeSensorActivationStatus(testSensor, true);
-
         verify(securityRepository, never()).setAlarmStatus(any(AlarmStatus.class));
     }
 
     @Test
     void shouldTriggerAlarmWhenActiveSensorReactivatedWhilePending() {
         Sensor sensor = new Sensor("Test Sensor", SensorType.DOOR);
-
         when(securityRepository.getAlarmStatus()).thenReturn(AlarmStatus.PENDING_ALARM);
         when(securityRepository.getSensors()).thenReturn(Set.of(sensor));
-
         securityService.changeSensorActivationStatus(sensor, true);
-
         verify(securityRepository).setAlarmStatus(AlarmStatus.ALARM);
     }
 
     @Test
     void shouldNotChangeStateWhenInactiveSensorDeactivated() {
         testSensor.setActive(false);
-
         securityService.changeSensorActivationStatus(testSensor, false);
-
         verify(securityRepository, never()).setAlarmStatus(any(AlarmStatus.class));
     }
 
@@ -106,10 +101,8 @@ class SecurityServiceTest {
     void shouldTriggerAlarmWhenCatDetectedAndSystemArmedHome() {
         when(securityRepository.getArmingStatus()).thenReturn(ArmingStatus.ARMED_HOME);
         when(imageService.imageContainsCat(any(), anyFloat())).thenReturn(true);
-
         BufferedImage image = new BufferedImage(100, 100, BufferedImage.TYPE_INT_RGB);
         securityService.processImage(image);
-
         verify(securityRepository).setAlarmStatus(AlarmStatus.ALARM);
     }
 
@@ -117,10 +110,8 @@ class SecurityServiceTest {
     void shouldResetToNoAlarmWhenNoCatDetectedAndAllSensorsInactive() {
         when(imageService.imageContainsCat(any(), anyFloat())).thenReturn(false);
         when(securityRepository.getSensors()).thenReturn(Set.of(testSensor));
-
         BufferedImage image = new BufferedImage(100, 100, BufferedImage.TYPE_INT_RGB);
         securityService.processImage(image);
-
         verify(securityRepository).setAlarmStatus(AlarmStatus.NO_ALARM);
     }
 
@@ -128,7 +119,6 @@ class SecurityServiceTest {
     @EnumSource(value = AlarmStatus.class, names = {"ALARM", "PENDING_ALARM"})
     void shouldResetToNoAlarmWhenSystemDisarmed(AlarmStatus alarmStatus) {
         securityService.setArmingStatus(ArmingStatus.DISARMED);
-
         verify(securityRepository).setAlarmStatus(AlarmStatus.NO_ALARM);
     }
 
@@ -138,27 +128,28 @@ class SecurityServiceTest {
         testSensor.setActive(true);
         Set<Sensor> sensors = new HashSet<>();
         sensors.add(testSensor);
-
         when(securityRepository.getSensors()).thenReturn(sensors);
-
         securityService.setArmingStatus(status);
-
-        assertFalse(testSensor.getActive());
+        
+        // Verify sensor was deactivated
         verify(securityRepository).updateSensor(testSensor);
+        assertFalse(testSensor.getActive(), "Sensor should be inactive after system is armed");
     }
 
     @Test
-    void shouldTriggerAlarmWhenSystemArmedHomeAndCatDetected() {
+    void shouldSetAlarmStatusToAlarmWhenArmedHomeWithCatPresent() {
+        // Setup image with cat
+        BufferedImage catImage = new BufferedImage(100, 100, BufferedImage.TYPE_INT_RGB);
+        when(imageService.imageContainsCat(any(), anyFloat())).thenReturn(true);
+        
+        // Process cat image while system is disarmed
         when(securityRepository.getArmingStatus())
                 .thenReturn(ArmingStatus.DISARMED)
                 .thenReturn(ArmingStatus.ARMED_HOME);
-        when(imageService.imageContainsCat(any(), anyFloat())).thenReturn(true);
-
-        BufferedImage image = new BufferedImage(100, 100, BufferedImage.TYPE_INT_RGB);
-        securityService.processImage(image);
-
+        securityService.processImage(catImage);
+        
+        // Change to ARMED_HOME and verify alarm is triggered
         securityService.setArmingStatus(ArmingStatus.ARMED_HOME);
-
         verify(securityRepository).setAlarmStatus(AlarmStatus.ALARM);
     }
 }
